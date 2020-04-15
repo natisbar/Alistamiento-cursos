1858c0118c1ba413c729e36ac7fbe4bea8848364
Natalia Barbosa <jandreaj100@gmail.com>
2020-04-15 18:08:11 -0500

    ajustes de reunion ajuste parcial

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,3 @@
-
 // Get the modal
 var modal = document.getElementById("myModal");
 var modal2 = document.getElementById("myModal2");
@@ -16,35 +15,35 @@ var span3 = document.getElementsByClassName("close")[2];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 btn2.onclick = function() {
-  modal2.style.display = "block";
+    modal2.style.display = "block";
 }
 btn3.onclick = function() {
-  modal3.style.display = "block";
+    modal3.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 span2.onclick = function() {
-  modal2.style.display = "none";
+    modal2.style.display = "none";
 }
 span3.onclick = function() {
-  modal3.style.display = "none";
+    modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-  else if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-  else if (event.target == modal3) {
-    modal3.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+//   else if (event.target == modal2) {
+//     modal2.style.display = "none";
+//   }
+//   else if (event.target == modal3) {
+//     modal3.style.display = "none";
+//   }
+// }
--- Js/script2.js
+++ Js/script2.js
@@ -25,62 +25,62 @@ var span5 = document.getElementsByClassName("close")[5];
 // When the user clicks the button, open the modal 
 
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 btn1.onclick = function() {
-  modal1.style.display = "block";
+    modal1.style.display = "block";
 }
 btn2.onclick = function() {
-  modal2.style.display = "block";
+    modal2.style.display = "block";
 }
 btn3.onclick = function() {
-  modal3.style.display = "block";
+    modal3.style.display = "block";
 }
 btn4.onclick = function() {
-  modal4.style.display = "block";
+    modal4.style.display = "block";
 }
 btn5.onclick = function() {
-  modal5.style.display = "block";
+    modal5.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 span1.onclick = function() {
-  modal1.style.display = "none";
+    modal1.style.display = "none";
 }
 span2.onclick = function() {
-  modal2.style.display = "none";
+    modal2.style.display = "none";
 }
 span3.onclick = function() {
-  modal3.style.display = "none";
+    modal3.style.display = "none";
 }
 span4.onclick = function() {
-  modal4.style.display = "none";
+    modal4.style.display = "none";
 }
 span5.onclick = function() {
-  modal5.style.display = "none";
+    modal5.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-  else if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
-  else if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-  else if (event.target == modal3) {
-    modal3.style.display = "none";
-  }
-  else if (event.target == modal4) {
-    modal4.style.display = "none";
-  }
-  else if (event.target == modal5) {
-    modal5.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+//   else if (event.target == modal1) {
+//     modal1.style.display = "none";
+//   }
+//   else if (event.target == modal2) {
+//     modal2.style.display = "none";
+//   }
+//   else if (event.target == modal3) {
+//     modal3.style.display = "none";
+//   }
+//   else if (event.target == modal4) {
+//     modal4.style.display = "none";
+//   }
+//   else if (event.target == modal5) {
+//     modal5.style.display = "none";
+//   }
+// }
