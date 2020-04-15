2251f4a8147a0508882e01d098047beaf7466265
Natalia Barbosa <jandreaj100@gmail.com>
2020-04-15 14:08:35 -0500

    ajuste diego a visualización de videos

---
+++ Js/cerrarModal.js
@@ -0,0 +1,11 @@
+function refresh() {
+    location.reload(true);
+}
+
+document.getElementById("closeModal_1").addEventListener("click", refresh);
+document.getElementById("closeModal_2").addEventListener("click", refresh);
+document.getElementById("closeModal_3").addEventListener("click", refresh);
+
+document.getElementById("closeModal_4").addEventListener("click", refresh);
+document.getElementById("closeModal_5").addEventListener("click", refresh);
+document.getElementById("closeModal_6").addEventListener("click", refresh);
