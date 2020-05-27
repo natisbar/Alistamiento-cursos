d9d7d6e10218c5e8b6dc9fb5ca5fa04da416b4b4
Natalia Barbosa <jandreaj100@gmail.com>
2020-05-27 09:22:44 -0500

    Ajuste ruta certificación

--- Js/script.js
+++ Js/script.js
@@ -2,16 +2,19 @@
 var modal = document.getElementById("myModal");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
 
 // Get the button that opens the modal
 var btn = document.getElementById("agenda");
 var btn2 = document.getElementById("enlaces");
 var btn3 = document.getElementById("guias");
+var btn4 = document.getElementById("syllabus");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
 var span3 = document.getElementsByClassName("close")[2];
+var span4 = document.getElementsByClassName("close")[3];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -23,6 +26,9 @@ btn2.onclick = function() {
 btn3.onclick = function() {
     modal3.style.display = "block";
 }
+btn4.onclick = function() {
+    modal4.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -33,6 +39,9 @@ span2.onclick = function() {
 }
 span3.onclick = function() {
     modal3.style.display = "none";
+}
+span4.onclick = function() {
+    modal4.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
